258d443ca27ec193b933937f733c4126754eee3a
max13582 <max13582721@gmail.com>
2024-03-29 11:21:59 +0800

    1.衝刺按鈕功能-製作了兩種操作模式(長壓與點擊切換ON/OFF模式) 2.BattleSceneUI移除不使用的元件與調整衝刺按鈕為單獨元件
    測試方法:
    1.場景直接開BattleScene執行
    2.Hierarchy打開UICanvas(Clone) > BattleSceneUI(Clone) > SprintBtn
    
    參數說明
    PressMode > 打勾為長壓模式(壓住衝刺 放開停止衝刺 Console會印訊息衝刺中或停止衝刺 沒打勾則是ON/OFF模式)

---
+++ gladiators/Assets/Scripts/Assembly_Game/BattleScene/UI/BattleSprintButton.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+
+public class BattleSprintButton : MonoBehaviour
+{
+    //兩種操作模式 做兩種用意是為了測試手感哪種比較好 決定用哪種之後再確定介面要怎麼演出
+    //1.長壓模式(就是傳統的Press)
+    //2.點擊切換On/off
+    //TODO:日後確定哪種操作方式後 要補上演出
+
+    [HeaderAttribute("==============TEST==============")]
+    [Tooltip("打勾為長壓模式 否則為ON/OFF模式 ON/OFF模式操作方式為點一下ON再點一下OFF")][SerializeField] bool PressMode = true;
+
+    [SerializeField] Text StateText;
+
+    bool IsOn = false; //On/Off模式 true為On false為Off 預設為off 
+
+    public void PressMode_Run()
+    {
+        if (PressMode)
+        {
+            Run();
+            HideText();
+        }
+    }
+
+    public void PressMode_Stop()
+    {
+        if (PressMode)
+        {
+            StopRun();
+            HideText();
+        }
+    }
+
+    public void OnOffMode_Click()
+    {
+        if(!PressMode)
+        {
+            IsOn = !IsOn;
+            if(IsOn)
+                OnOffMode_Run();
+            else
+                OnOffMode_Stop();
+        }
+    }
+
+    void OnOffMode_Run()
+    {
+        if (!PressMode)
+        {
+            Run();
+            OnText();
+        }            
+    }
+
+    void OnOffMode_Stop()
+    {
+        if(!PressMode)
+        {
+            StopRun();
+            OffText();
+        }
+    }
+
+    public void OnText()
+    {
+        StateText.text = "衝刺中";
+    }
+
+    public void OffText()
+    {
+        StateText.text = "行走中";
+    }
+
+    public void HideText()
+    {
+        StateText.text = "";
+    }
+
+    public void Run()
+    {
+        Debug.Log("衝刺中~~~");
+    }
+
+    public void StopRun()
+    {
+        Debug.Log("停止衝刺~~~~");
+    }
+}
